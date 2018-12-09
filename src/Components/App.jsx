4a56a4cdc2346bae2a92689fe66fe63a89cef679
RiCK BROWN <ricky.brown.00@gmail.com>
2018-12-09 20:30:48 +0000

    Installed semantic ui

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,6 +5,7 @@ class App extends Component {
     return (
       <div>
         banterHero
+        
       </div>
     )
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+// import '../semantic/dist/semantic.min.css'
 import './index.css'
-import App from './App'
+import App from './Components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const rootElement = document.getElementById('root')
+
+let render = ( )=> {
+  ReactDOM.render(<App />, rootElement)
+}
+
+if(module.hot) {
+  module.hot.accept('./Components/App', () => {
+    setTimeout(render)
+  })
+}
+
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
