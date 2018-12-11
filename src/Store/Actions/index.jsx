0dc38ee268342e22d40b487db1ba11664a3cdb65
RiCK BROWN <ricky.brown.00@gmail.com>
2018-12-11 12:50:52 +0000

    Added store data

---
+++ src/Store/Actions/index.jsx
@@ -0,0 +1,2 @@
+import { databaseRef, authRef } from '../Config/firebase'
+import { FETCH_BANTER, FETCH_USER } from '../../Constants/types'
---
+++ src/Store/Config/keys.jsx
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyALkIJ1ZMKyr4iwPhq5x91IzUIc1wk-Z5I',
+  authDomain: 'fir-sandbox-5dfb0.firebaseapp.com',
+  databaseURL: 'https://fir-sandbox-5dfb0.firebaseio.com',
+  projectId: 'fir-sandbox-5dfb0',
+  storageBucket: 'fir-sandbox-5dfb0.appspot.com',
+  messagingSenderId: '109795635169',
+}
